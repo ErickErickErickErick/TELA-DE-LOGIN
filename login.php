0d27f580989bc2b809201407af028ac964e45e1f
ErickErickErickErick <erick_braatz@estudante.sesisenai.org.br>
2023-04-05 09:30:02 -0300

    first commit

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $login = $_POST["login"];
+    $senha = $_POST["senha"];
+
+    echo($login);
+    echo("<br>");
+    echo($senha);
+?>
+</body>
+</html>
+
+
+
+
